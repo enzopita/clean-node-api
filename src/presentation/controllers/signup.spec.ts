305966404b5b28415fcab7432e2a48fecd500586
Enzo Pita <46866054+enzopita@users.noreply.github.com>
2023-01-10 06:06:09 -0300

    test: ensure SignUpController returns 400 if no name is provided

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,17 @@
+import { describe, expect, test } from 'vitest';
+import { SignUpController } from './signup';
+
+describe('SignUp Controller', () => {
+  test('Should return 400 if no name is provided', () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        email: 'email@company.com',
+        password: 'password',
+        passwordConfirmation: 'password',
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+  });
+});
